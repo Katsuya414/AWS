6d81de683b2398ebf33e33408c84dca28ddc6be2
katsuya <katu7414@gmail.com>
2019-05-27 14:10:20 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-variable "access_key" {}
-variable "secret_key" {}
+variable "aws_access_key_id" {}
+variable "aws_secret_access_key" {}
 variable "region" {
   default = "us-west-2"
 }
@@ -8,4 +8,11 @@ provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
     region = "${var.region}"
+}
+
+resource "aws_vpc" "vpc-1" {
+  cidr_block = "10.0.0.0/16"
+  tags {
+    Name = "vpc-1"
+  }
 }
