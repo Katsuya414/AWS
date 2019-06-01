056b594fbe56353d095f229129e2cf57dbd6444b
katsuya <katu7414@gmail.com>
2019-06-01 22:14:16 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "vpc-1" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "10.3.0.0/16"
   tags = {
     Name = "vpc-1"
   }
@@ -22,11 +22,17 @@ resource "aws_eip" "nat" {
     vpc = true
 }
 
+resource "aws_eip" "nat2" {
+    vpc = true
+}
+
 # NatGateway
 resource "aws_nat_gateway" "nat" {
   allocation_id = "${aws_eip.nat.id}"
-  subnet_id = "${aws_subnet.public-a.id}"
+  subnet_id = "${aws_subnet.private-a.id}"
 }
+
+
 
 # InternetGateway
 resource "aws_internet_gateway" "igw" {
@@ -39,7 +45,7 @@ resource "aws_subnet" "public-a" {
   vpc_id = "${aws_vpc.vpc-1.id}"
   cidr_block = "10.3.0.0/24"
   availability_zone = "us-west-2a"
-  tags {
+  tags = {
     Name = "public-a"
   }
 }
@@ -48,7 +54,7 @@ resource "aws_subnet" "public-c" {
   vpc_id = "${aws_vpc.vpc-1.id}"
   cidr_block = "10.3.2.0/24"
   availability_zone = "us-west-2c"
-  tags {
+  tags = {
     Name = "public-c"
   }
 }
@@ -57,7 +63,7 @@ resource "aws_subnet" "private-a" {
   vpc_id = "${aws_vpc.vpc-1.id}"
   cidr_block = "10.3.4.0/24"
   availability_zone = "us-west-2a"
-  tags {
+  tags = {
     Name = "private-a"
   }
 }
@@ -66,7 +72,51 @@ resource "aws_subnet" "private-c" {
   vpc_id = "${aws_vpc.vpc-1.id}"
   cidr_block = "10.3.6.0/24"
   availability_zone = "us-west-2c"
-  tags {
+  tags = {
     Name = "private-c"
   }
+}
+
+# RouteTable
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.vpc-1.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw.id}"
+  }
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = "${aws_vpc.vpc-1.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = "${aws_nat_gateway.nat.id}"
+  }
+  tags = {
+    Name = "private"
+  }
+}
+
+# SubnetRouteTableAssociation
+resource "aws_route_table_association" "public-a" {
+    subnet_id = "${aws_subnet.public-a.id}"
+    route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_route_table_association" "public-c" {
+    subnet_id = "${aws_subnet.public-c.id}"
+    route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_route_table_association" "private-a" {
+    subnet_id = "${aws_subnet.private-a.id}"
+    route_table_id = "${aws_route_table.private.id}"
+}
+
+resource "aws_route_table_association" "private-c" {
+    subnet_id = "${aws_subnet.private-c.id}"
+    route_table_id = "${aws_route_table.private.id}"
 }
